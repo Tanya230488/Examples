c7d79cd07bef7c58aac51e9fda55bfc751e1a629
Tanya <felisitas@bk.ru>
2023-08-21 14:01:36 +0700

    add exercise 1

---
+++ Ex.1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write ("Введите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write ("Введите квадрат числа: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number2 == number1 * number1) Console.WriteLine($"Число {number2} является квадратом числа {number1}");
+else Console.WriteLine($"Число {number2} не является квадратом числа {number1}"); 
