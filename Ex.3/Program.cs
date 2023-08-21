8b053d78052bfd09985994900ba5c7dc2fd238a3
Tanya <felisitas@bk.ru>
2023-08-21 18:30:53 +0700

    add ex.3

---
+++ Ex.3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int third = number %10;
+int second = number /10 %10;
+int first = number /100; 
+
+Console.WriteLine ($"Первая цифра числа {number} равна {first}");
+Console.WriteLine ($"Вторая цифра числа {number} равна {second}");
+Console.WriteLine ($"Третья цифра числа {number} равна {third}");
